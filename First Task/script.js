b09d4f775e585a9872b4e27250c9c0da23c8bf39
djonny87 <45122167+djonny87@users.noreply.github.com>
2019-09-06 17:45:47 +0300

    teskTask

---
+++ First Task/script.js
@@ -0,0 +1,50 @@
+// открыть по кнопке
+$('.js-button-campaign').click(function() { 
+	
+	$('.js-overlay-campaign').fadeIn();
+	$('.js-overlay-campaign').addClass('disabled');
+});
+
+// закрыть на крестик
+$('.js-close-campaign').click(function() { 
+	$('.js-overlay-campaign').fadeOut();
+	
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+	var popup = $('.js-popup-campaign');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-campaign').fadeOut();
+		
+	}
+});
+
+// открыть по таймеру 
+$(window).on('load', function () { 
+	setTimeout(function(){
+		if($('.js-overlay-campaign').hasClass('disabled')) {
+			return false;
+		} else {
+			
+			$(".js-overlay-campaign").fadeIn();
+		}
+	}, 5000);
+});
+
+
+function maxLengthCheck(object) {
+    if (object.value.length > object.maxLength)
+      object.value = object.value.slice(0, object.maxLength)
+  }
+    
+  function isNumeric (evt) {
+    var theEvent = evt || window.event;
+    var key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode (key);
+    var regex = /[0-9]|\./;
+    if ( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+}
